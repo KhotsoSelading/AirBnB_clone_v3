8d6653f32ab9103dc8396a5a0a1de3a1a9b27970
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-10-29 17:34:05 +0200

    pycodestyle

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 """
-A view for Review instances that manages all standard
-RESTful API actions.
+A view for Review instances that manages all standard RESTful API actions.
 
 Authors:Khotso Selading and Londeka Dlamini
 """
